f33e9d1812969922c703c27d805cbb7ff6acd0d8
Ronaldo Santos <ronaldo100timo@gmail.com>
2020-07-12 18:50:53 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/ronaldosantos/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.ronaldosantos.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ronaldosantos.domain.Categoria;
+import com.ronaldosantos.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+	Optional<Categoria> obj = repo.findById(id);
+		
+	return obj.orElse(null); 
+
+	}
+	
+	
+}
