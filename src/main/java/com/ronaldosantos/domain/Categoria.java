6f69f17020663d15185a60b18b518667d565d581
Ronaldo Santos <ronaldo100timo@gmail.com>
2020-07-12 18:10:15 -0300

    Banco de dados H2 e a criacao automatica da base de dados

--- src/main/java/com/ronaldosantos/domain/Categoria.java
+++ src/main/java/com/ronaldosantos/domain/Categoria.java
@@ -2,10 +2,17 @@ package com.ronaldosantos.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
@@ -60,8 +67,4 @@ public class Categoria implements Serializable {
 		return true;
 	}
 
-	
-	
-	
-	
 }
