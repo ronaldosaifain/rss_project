1058eb887282050f6808faabd55e74b496bb2986
Ronaldo Santos <ronaldo100timo@gmail.com>
2020-07-23 23:36:13 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/ronaldosantos/domain/ItemPedidoPK.java
+++ src/main/java/com/ronaldosantos/domain/ItemPedidoPK.java
@@ -3,7 +3,6 @@ package com.ronaldosantos.domain;
 import java.io.Serializable;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Embedded;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
