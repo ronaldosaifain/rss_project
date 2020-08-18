349dfbb57ec812a3741b33ade990d65460dedf26
Ronaldo Santos <ronaldo100timo@gmail.com>
2020-08-17 22:25:42 -0300

    Correcoes update em categoria service e construtor de pagamento

--- src/main/java/com/ronaldosantos/resources/ClienteResource.java
+++ src/main/java/com/ronaldosantos/resources/ClienteResource.java
@@ -18,8 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ronaldosantos.domain.Cliente;
-import com.ronaldosantos.domain.Cliente;
-import com.ronaldosantos.dto.ClienteDTO;
 import com.ronaldosantos.dto.ClienteDTO;
 import com.ronaldosantos.services.ClienteService;
 
--- src/main/java/com/ronaldosantos/services/CategoriaService.java
+++ src/main/java/com/ronaldosantos/services/CategoriaService.java
@@ -37,8 +37,10 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
 	}
 	
 	public void delete(Integer id) {
@@ -65,5 +67,11 @@ public class CategoriaService {
 	return new 	Categoria(objDto.getId(), objDto.getNome());
 		
 	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		
+	}
+	
 	
 }
